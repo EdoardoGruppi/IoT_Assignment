4ff46c5632738f2607d5cd738c2f41273abecf07
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-02-13 23:38:17 +0100

    First functions created for the dataset organization

--- Modules/utilities.py
+++ Modules/utilities.py
@@ -1,1 +1,22 @@
 # Import packages
+from pandas import DatetimeIndex, to_datetime, date_range
+
+
+def get_info(dataframe):
+    print('\nDataframe info ' + '=' * 30)
+    print(dataframe.info())
+    # todo types of variables
+
+
+def remove_features(dataframe):
+    columns_names = ['time', 'Total Energy ', 'Generated Energy', 'House Overall', 'Dishwasher', 'Furnace 1',
+                     'Furnace 2', 'Home Office', 'Fridge', 'Wine Cellar', 'Garage Door', 'Kitchen 1', 'Kitchen 2',
+                     'Kitchen 3', 'Barn', 'Well', 'Microwave', 'Living Room', 'Solar', 'Temperature', 'Light',
+                     'Humidity', 'visibility', 'summary', 'apparentTemperature', 'Pressure', 'Wind Speed', 'cloudCover',
+                     'Wind Bearing', 'Precipitation', 'Dew Point', 'precipProbability']
+    dataframe.columns = columns_names
+    start_date = to_datetime(dataframe['time'][0], unit='s').strftime('%Y-%m-%d %H:%M')
+    dataframe['time'] = date_range(start_date, periods=len(dataframe), freq='min')
+    dataframe = dataframe.set_index(DatetimeIndex(dataframe['time']))
+    columns_to_remove = ['time', 'visibility', 'summary', 'apparentTemperature', 'cloudCover', 'precipProbability']
+    return dataframe.drop(columns_to_remove, axis=1)
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # Import packages
-
+from pandas import read_csv
+import os
+from Modules.config import *
+from Modules.utilities import *
 
 # DATA ACQUISITION =====================================================================================================
 
 
 # DATA PREPROCESSING ===================================================================================================
-
-
+dataframe = read_csv(os.path.join(base_dir, 'HomeC.csv'), sep=',')
+dataframe = remove_features(dataframe)
+get_info(dataframe)
+print(dataframe.head())
+print(dataframe.tail())
 # DATA EXPLORATION AND HYPOTHESIS TESTING ==============================================================================
 
 
