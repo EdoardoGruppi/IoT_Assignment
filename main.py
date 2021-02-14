a0bdbc8a0d2011c1b15e89e971d7d38c66a6003d
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-02-15 00:25:41 +0100

    Minor amendments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pandas import read_csv
 import os
 from Modules.config import *
 from Modules.utilities import *
+from Modules.visualization import *
 
 # DATA ACQUISITION =====================================================================================================
 
@@ -10,10 +11,14 @@ from Modules.utilities import *
 # DATA PREPROCESSING ===================================================================================================
 dataframe = read_csv(os.path.join(base_dir, 'HomeC.csv'), sep=',')
 dataframe = remove_features(dataframe)
-get_info(dataframe)
-print(dataframe.head())
-print(dataframe.tail())
+# get_info(dataframe)
+dataframe = get_time_details(dataframe)
+
+
 # DATA EXPLORATION AND HYPOTHESIS TESTING ==============================================================================
+# todo remove features for multicollinearity
+plot_correlation_matrix(dataframe)
+plot_distribution(dataframe, x='Total Energy', hue='Month')
 
 
 # DATA INFERENCE =======================================================================================================
