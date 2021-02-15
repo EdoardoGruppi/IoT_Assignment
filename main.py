2a95a073ca3b6bf6dd883c958bb2948b95889aa0
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-02-15 15:59:06 +0100

    Change code to visualize data

--- main.py
+++ main.py
@@ -14,12 +14,18 @@ dataframe = remove_features(dataframe)
 # get_info(dataframe)
 dataframe = get_time_details(dataframe)
 
-
 # DATA EXPLORATION AND HYPOTHESIS TESTING ==============================================================================
-# todo remove features for multicollinearity
-plot_correlation_matrix(dataframe)
-plot_distribution(dataframe, x='Total Energy', hue='Month')
+# plot_correlation_matrix(dataframe)
+# plot_distribution(dataframe, x='Total Energy', hue='Month')
+# dataframe = detect_univariate_outlier(dataframe.iloc[:, :2], cap='z_score', nan=None)
+# scatter_plot_matrix(dataframe.iloc[:, :5])
+# plot_distributions(dataframe.iloc[:, :20], columns=5)
+# box_plot(dataframe, dataframe.columns[:5])
+# hue_scatter_plot_matrix(dataframe, dataframe.columns[:2], ['Month'])
+# detect_seasonality(dataframe, dataframe.columns[0], 'Hour', 'Week Day')
+# todo Remove features for multicollinearity
+
+# PREPARE DATASET ======================================================================================================
 
 
 # DATA INFERENCE =======================================================================================================
-
