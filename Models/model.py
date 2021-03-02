e7918528f4a15fb4b427a0f68c9045be5ea95b79
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-03-02 23:37:48 +0100

    Insert LGBM regressor

--- Models/model.py
+++ Models/model.py
@@ -1,12 +1,10 @@
 # Import packages
-import numpy as np
-from sklearn.svm import SVR, LinearSVR
-from sklearn.model_selection import TimeSeriesSplit, GridSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.svm import LinearSVR
+from sklearn.model_selection import GridSearchCV
 from Modules.visualization import plot_results
 from xgboost import XGBRegressor, XGBRFRegressor
 from Modules.utilities import compute_metrics
+from lightgbm import LGBMRegressor
 
 
 def support_vector_machine(train, train_target, test, test_target, plot=True, cv=5):
@@ -26,7 +24,7 @@ def support_vector_machine(train, train_target, test, test_target, plot=True, cv
     params = {'C': [1, 5, 10, 50, 100]}
     # Instance of the model
     model_SVR = LinearSVR(max_iter=10000, loss='squared_epsilon_insensitive', dual=False)
-    grids = GridSearchCV(model_SVR, params, cv=cv, verbose=3, n_jobs=-1)
+    grids = GridSearchCV(model_SVR, params, cv=cv, verbose=1, n_jobs=-1)
     # Search the best values
     grids.fit(train, train_target)
     print(f'Best estimator: {grids.best_estimator_}')
@@ -55,10 +53,10 @@ def xgb_regressor(train, train_target, test, test_target, plot=True, cv=5):
     :return: the features importance.
     """
     # List of parameters to evaluate
-    params = {'n_estimators': [1, 3, 5], 'max_depth': [4, 8, 12], 'learning_rate': [0.01, 0.1, 1]}
+    params = {'n_estimators': [5, 8, 10], 'max_depth': [10, 15, 20], 'learning_rate': [0.01, 0.1, 1]}
     # Instantiate a XGBRegressor object
     model_XGB = XGBRegressor()
-    grids = GridSearchCV(model_XGB, params, cv=cv, verbose=3, n_jobs=-1)
+    grids = GridSearchCV(model_XGB, params, cv=cv, verbose=1, n_jobs=-1)
     # Fit the model according to the given training data
     grids.fit(train, train_target)
     print(f'Best estimator: {grids.best_estimator_}')
@@ -87,10 +85,10 @@ def xgb_random_forest_regressor(train, train_target, test, test_target, plot=Tru
     :return: the features importance.
     """
     # List of parameters to evaluate
-    params = {'n_estimators': [1, 3, 5], 'max_depth': [4, 8, 12], 'learning_rate': [0.01, 0.1, 1]}
+    params = {'n_estimators': [5, 10, 15], 'max_depth': [10, 15, 20], 'learning_rate': [0.5, 1]}
     # Instantiate a XGBRFRegressor object
     model_XGB_RF = XGBRFRegressor()
-    grids = GridSearchCV(model_XGB_RF, params, cv=cv, verbose=3, n_jobs=-1, refit=True)
+    grids = GridSearchCV(model_XGB_RF, params, cv=cv, verbose=1, n_jobs=-1, refit=True)
     # Fit the model according to the given training data
     grids.fit(train, train_target)
     print(f'Best estimator: {grids.best_estimator_}')
@@ -104,3 +102,36 @@ def xgb_random_forest_regressor(train, train_target, test, test_target, plot=Tru
     if plot:
         plot_results(test_predictions, test_target)
     return model_XGB_RF
+
+
+def light_gbm_regressor(train, train_target, test, test_target, plot=True, cv=5):
+    """
+    Trains a Light GBM Regressor model to predict a target variable.
+
+    :param train: train dataset.
+    :param train_target: target column related to the training dataset.
+    :param test: test dataset.
+    :param test_target: target column related to the test dataset.
+    :param plot: if True the results are plotted as well. default_Value=True
+    :param cv: determines the number of folds used in the cross-validation splitting strategy. default_Value=5
+    :return: the features importance.
+    """
+    # List of parameters to evaluate
+    params = {'n_estimators': [5, 8, 10], 'max_depth': [10, 15, 20], 'learning_rate': [0.01, 0.1, 1],
+              'min_child_samples': [2, 3, 5]}
+    # Instantiate a LGBMRegressor object
+    model_LGBM = LGBMRegressor(num_leaves=200)
+    grids = GridSearchCV(model_LGBM, params, cv=cv, verbose=1, n_jobs=-1, refit=True)
+    # Fit the model according to the given training data
+    grids.fit(train, train_target)
+    print(f'Best estimator: {grids.best_estimator_}')
+    # Train the best estimator
+    model_LGBM = grids.best_estimator_
+    model_LGBM.fit(train, train_target)
+    # Predict target variable for samples in the test dataset
+    test_predictions = model_LGBM.predict(test)
+    # Compute and print metrics
+    compute_metrics(test_target, test_predictions, model='LGBM')
+    if plot:
+        plot_results(test_predictions, test_target)
+    return model_LGBM
